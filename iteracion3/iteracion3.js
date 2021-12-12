37bedf3585d5adf0330ab427c441c6d468ae7617
AlejandroRDev <iron-man@avengers.com>
2021-12-12 14:22:21 +0100

    #Iteration-3

---
+++ iteracion3/iteracion3.js
@@ -0,0 +1,32 @@
+//1.1
+
+x = 10 * 5;
+
+console.log(x);
+
+//1.2
+
+x = 10 / 2;
+
+console.log(x);
+
+//1.3
+
+x = 15 % 9;
+
+console.log(x);
+
+//1.4
+
+y = 10;
+z = 5;
+
+x = y + z;
+
+console.log(x);
+
+//1.5
+
+x = y * z;
+
+console.log(x);
